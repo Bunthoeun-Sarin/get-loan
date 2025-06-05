9f39b030857fab55bba141595c975a44bbd4e11e
Bunthoeun-Sarin <bunthoeunsarin@gmail.com>
2025-06-05 23:05:21 +0700

    fixed all error

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = encodeURIComponent(document.getElementById("name").value);
+
+  fetch(
+    `https://script.google.com/macros/s/AKfycbyFY9YwFJRbndgDbgKwMc_K5QaPmzt6PyJa6EGeBAGb014j10XMQ0mL9WcrrXswwAPoUQ/exec?name=${name}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      alert("Server response: " + data.status + ", Name: " + data.received);
+    })
+    .catch((err) => {
+      console.error("Fetch error:", err);
+    });
+});
